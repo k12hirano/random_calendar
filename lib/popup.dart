14b4937ca42cbc52b24d13f898327257d27a36c0
k12hirano <k12hirano@gmail.com>
2021-11-10 01:28:43 +0900

    4th commit

--- lib/popup.dart
+++ lib/popup.dart
@@ -10,11 +10,11 @@ class Popup extends StatefulWidget {
 class _PopupState extends State<Popup> {
   List _eventLists =[];
   var todotext = TextEditingController();
-  bool han;
-  bool ichi;
-  bool tan;
-  bool one;
-  bool multi;
+  bool han= false;
+  bool ichi = false;
+  bool tan = false;
+  bool one = false;
+  bool multi = false;
 
 
   @override
@@ -28,7 +28,7 @@ class _PopupState extends State<Popup> {
     final width = MediaQuery.of(context).size.width;
     final adjustsizeh = MediaQuery.of(context).size.height * 0.0011;
 
-    Future<Widget> _todoDialog(BuildContext context) async {
+   /* Future<Widget> _todoDialog(BuildContext context) async {
       await showDialog(
           context: context,
           builder: (_) {
@@ -41,9 +41,10 @@ class _PopupState extends State<Popup> {
                   body: Container(
                       child: Column(children: <Widget>[
                         Container(
+                          height: height*0.15,
                             child: TextFormField(decoration: InputDecoration(
                                 isDense: true,
-                                hintText: 'やること入力してください'),
+                                hintText: 'やってみたいこと入力してください'),
                                 controller: todotext,
                                 style: TextStyle(
                                     fontSize: 15*adjustsizeh,
@@ -51,22 +52,35 @@ class _PopupState extends State<Popup> {
                                     color: Colors.brown
                                 )
                             )),
-                        Container(child: Column(children: <Widget>[
+                        Container(
+                            height: height*0.15,
+                            child: Column(children: <Widget>[
                           Text('規模選択'),
-                          Container(child:Row(children: <Widget>[
-                            Container(child: Row(children: <Widget>[
+                          Container(
+                              height: height*0.1,
+                              child:Row(children: <Widget>[
+                            Container(
+                                height: height*0.1,
+                                width: width*0.23,
+                                child: Row(children: <Widget>[
                               Checkbox(value: han, onChanged: (value){
                                 han=value;
                               }),
                               Text('半日')
                             ])),
-                            Container(child: Row(children: <Widget>[
+                            Container(
+                                height: height*0.1,
+                                width: width*0.23,
+                                child: Row(children: <Widget>[
                               Checkbox(value: ichi, onChanged: (value){
                                 ichi=value;
                               }),
                               Text('一日')
                             ])),
-                            Container(child: Row(children: <Widget>[
+                            Container(
+                                height: height*0.1,
+                                width: width*0.23,
+                                child: Row(children: <Widget>[
                               Checkbox(value: tan, onChanged: (value){
                                 tan=value;
                               }),
@@ -74,16 +88,26 @@ class _PopupState extends State<Popup> {
                             ]))
                           ]))
                         ])),
-                        Container(child: Column(children: <Widget>[
+                        Container(
+                          height: height*0.15,
+                          child: Column(children: <Widget>[
                           Text('回数選択'),
-                          Container(child:Row(children: <Widget>[
-                            Container(child: Row(children: <Widget>[
+                          Container(
+                              height: height*0.1,
+                              child:Row(children: <Widget>[
+                            Container(
+                                height: height*0.1,
+                                width: width*0.28,
+                                child: Row(children: <Widget>[
                               Checkbox(value: one, onChanged: (value){
                                 one=value;
                               }),
                               Text('１回のみ')
                             ])),
-                            Container(child: Row(children: <Widget>[
+                            Container(
+                                height: height*0.1,
+                                width: width*0.28,
+                                child: Row(children: <Widget>[
                               Checkbox(value: multi, onChanged: (value){
                                 multi=value;
                               }),
@@ -91,24 +115,181 @@ class _PopupState extends State<Popup> {
                             ])),
                             //TODO スイッチ形式で二択用意、　複数回選択時詳細設定部分表示し、回数指定（バーで選択or入力、不定期、月一など選択可）
                           ]))
-                        ]),),
-                        Container(child:Row(
+                        ])),
+                        Container(
+                            height: height*0.2,
+                            child:Row(
                             children: <Widget>[
-                              Container(child:ElevatedButton(child: Text('登録'))),
-                              Container(child: ElevatedButton(child: Text('キャンセル')))]
+                              Container(
+                                  height: height*0.17,
+                                  width: width*0.28,
+                                  child:ElevatedButton(
+                                  child: Text('登録', style: TextStyle(color: Colors.yellow[200],fontSize: 15)),
+                                  style: ElevatedButton.styleFrom(
+                                primary: Colors.lightGreen,
+                              ))),
+                              Container(
+                                  height: height*0.17,
+                                  width: width*0.28,
+                                  child: ElevatedButton(
+                                  child: Text('キャンセル',  style: TextStyle(color: Colors.yellow[200], fontSize: 15)),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.lightGreen,
+                                  )))]
                         ))
                       ])
                   ),
                 ),
               ),
             );
-          });}
+          });}*/
 
     Widget floatButton(){
       return FloatingActionButton(child: Icon(Icons.add,color: Colors.white),
           backgroundColor: Colors.lightGreen,
           onPressed: () async {
-            _todoDialog;
+            var result =  await showDialog(
+                context: context,
+                builder: (_) {
+                  return SimpleDialog(
+                    contentPadding: EdgeInsets.all(0.0),
+                    titlePadding: EdgeInsets.all(0.0),
+                    title: Container(
+                      height: height*0.65,
+                      child: Scaffold(
+                        body: Container(
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                              Container(
+                                  height: height*0.15,
+                                  child: TextFormField(decoration: InputDecoration(
+                                      isDense: true,
+                                      hintText: 'やってみたいこと入力してください'),
+                                      controller: todotext,
+                                      style: TextStyle(
+                                          fontSize: 15*adjustsizeh,
+                                          height: 2.0,
+                                          color: Colors.brown
+                                      )
+                                  )),
+                              Container(
+                                  height: height*0.15,
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                    Text('規模選択'),
+                                    Container(
+                                        height: height*0.1,
+                                        child:Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: <Widget>[
+                                          Container(
+                                              height: height*0.1,
+                                              width: width*0.23,
+                                              child: Row(
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: <Widget>[
+                                                Checkbox(value: han, onChanged: (value){
+                                                  han=value;
+                                                }),
+                                                Text('半日')
+                                              ])),
+                                          Container(
+                                              height: height*0.1,
+                                              width: width*0.23,
+                                              child: Row(
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: <Widget>[
+                                                Checkbox(value: ichi, onChanged: (value){
+                                                  ichi=value;
+                                                }),
+                                                Text('一日')
+                                              ])),
+                                          Container(
+                                              height: height*0.1,
+                                              width: width*0.23,
+                                              child: Row(
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: <Widget>[
+                                                Checkbox(value: tan, onChanged: (value){
+                                                  tan=value;
+                                                }),
+                                                Text('短時間')
+                                              ]))
+                                        ]))
+                                  ])),
+                              Container(
+                                  height: height*0.15,
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                    Text('回数選択'),
+                                    Container(
+                                        height: height*0.1,
+                                        child:Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: <Widget>[
+                                          Container(
+                                              height: height*0.1,
+                                              width: width*0.28,
+                                              child: Row(children: <Widget>[
+                                                Checkbox(value: one, onChanged: (value){
+                                                  one=value;
+                                                }),
+                                                Text('１回のみ')
+                                              ])),
+                                          Container(
+                                              height: height*0.1,
+                                              width: width*0.28,
+                                              child: Row(
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: <Widget>[
+                                                Checkbox(value: multi, onChanged: (value){
+                                                  multi=value;
+                                                }),
+                                                Text('複数回')
+                                              ])),
+                                          //TODO スイッチ形式で二択用意、　複数回選択時詳細設定部分表示し、回数指定（バーで選択or入力、不定期、月一など選択可）
+                                        ]))
+                                  ])),
+                              Container(
+                                  height: height*0.2,
+                                  child:Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        Container(
+                                            height: height*0.17,
+                                            width: width*0.28,
+                                            child:ElevatedButton(
+                                                child: Text('登録', style: TextStyle(color: Colors.yellow[200],fontSize: 15)),
+                                                style: ElevatedButton.styleFrom(
+                                                  primary: Colors.lightGreen,
+                                                ),
+                                                onPressed: () {
+
+                                                })),
+                                        Container(
+                                            height: height*0.17,
+                                            width: width*0.28,
+                                            child: ElevatedButton(
+                                                child: Text('キャンセル',  style: TextStyle(color: Colors.yellow[200], fontSize: 15)),
+                                                style: ElevatedButton.styleFrom(
+                                                  primary: Colors.lightGreen,
+                                                ),
+                                                onPressed: (){
+
+                                                }))]
+                                  ))
+                            ])
+                        ),
+                      ),
+                    ),
+                  );
+                });
       });
     }
     
@@ -137,7 +318,11 @@ class _PopupState extends State<Popup> {
               width: width*0.98,
             child: Column(
                 children: [
-                  todoLists()])
+                  Container(
+                      height: height*0.65,
+                      width: width*0.95,
+                      child:todoLists())
+                  ])
             )));
   }
 
